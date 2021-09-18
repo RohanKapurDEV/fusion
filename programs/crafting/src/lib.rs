ece9a5bf907b9f6908614054cafb88dde18cfc36
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-09-17 21:48:57 -0700

    add some basic test against the formula

--- programs/crafting/src/lib.rs
+++ programs/crafting/src/lib.rs
@@ -9,50 +9,51 @@ pub mod crafting {
     use super::*;
 
     pub fn create_formula(
-        ctx: Context<CreateFormula>,
-        items: Vec<Ingredient>,
-        reversible: bool,
-        bump: u8,             // Run `find_program_address` offchain for canonical bump
-        _space_multiple: u16, // The number of ingredients in the formula
+        _ctx: Context<CreateFormula>,
+        _ingredients_count: u16, // The number of ingredients in the formula
+        _items_count: u16, // The number of items output by the formula
+        _ingredients: Vec<Ingredient>,
+        _output_items: Vec<Item>,
+        // bump: u8,             // Run `find_program_address` offchain for canonical bump
     ) -> ProgramResult {
-        let formula = &mut ctx.accounts.formula;
-        formula.items = items;
-        formula.output_mint = *ctx.accounts.output_mint.to_account_info().key;
-        formula.reversible = reversible;
+        // let formula = &mut ctx.accounts.formula;
+        // formula.items = items;
+        // formula.output_mint = *ctx.accounts.output_mint.to_account_info().key;
+        // formula.reversible = reversible;
 
-        // Hand over control of the mint account to PDA
-        let pda_pubkey = Pubkey::create_program_address(
-            &[
-                &"crafting".as_bytes(),
-                &formula.to_account_info().key.to_bytes()[..32],
-                &[bump],
-            ],
-            &ctx.program_id,
-        )?;
+        // // Hand over control of the mint account to PDA
+        // let pda_pubkey = Pubkey::create_program_address(
+        //     &[
+        //         &"crafting".as_bytes(),
+        //         &formula.to_account_info().key.to_bytes()[..32],
+        //         &[bump],
+        //     ],
+        //     &ctx.program_id,
+        // )?;
 
-        let cpi_accounts = SetAuthority {
-            account_or_mint: ctx.accounts.output_mint.to_account_info().clone(),
-            current_authority: ctx.accounts.authority.clone(),
-        };
+        // let cpi_accounts = SetAuthority {
+        //     account_or_mint: ctx.accounts.output_mint.to_account_info().clone(),
+        //     current_authority: ctx.accounts.authority.clone(),
+        // };
 
-        let cpi_ctx = CpiContext::new(ctx.accounts.token_program.clone(), cpi_accounts);
-        set_authority(cpi_ctx, AuthorityType::MintTokens.into(), Some(pda_pubkey))?;
+        // let cpi_ctx = CpiContext::new(ctx.accounts.token_program.clone(), cpi_accounts);
+        // set_authority(cpi_ctx, AuthorityType::MintTokens.into(), Some(pda_pubkey))?;
 
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-#[instruction(_space_multiple: u16)]
+#[instruction(ingredients_count: u16, items_count: u16)]
 pub struct CreateFormula<'info> {
-    #[account(init, payer = authority, space = 8 + 32 + 1 + 33 * _space_multiple as usize)]
+    #[account(init,
+        payer = authority,
+        space = 8 + 32 + 1 + 34 * ingredients_count as usize + 33 * items_count as usize
+    )]
     pub formula: Account<'info, Formula>,
 
     #[account(mut)]
-    pub output_mint: Account<'info, Mint>,
-
-    #[account(signer)]
-    pub authority: AccountInfo<'info>,
+    pub authority: Signer<'info>,
 
     // System program account
     pub system_program: Program<'info, System>,
@@ -65,19 +66,28 @@ pub struct CreateFormula<'info> {
 pub struct Formula {
     // Vector of <Ingredient> items required to satisy the formula
     // Each <Ingredient> item is 33 bytes
-    pub items: Vec<Ingredient>,
+    pub ingredients: Vec<Ingredient>,
     // Pubkey of the mint of the formula
-    pub output_mint: Pubkey,
-    // Is formula reversible
-    pub reversible: bool,
+    pub output_items: Vec<Item>
+}
+
+/// Size: 32 + 1 + 1 = 34 bytes
+#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct Ingredient {
+    /// Pubkey of the ingredient's token mint
+    pub mint: Pubkey,
+    /// Amount of the token required to satisy the "creation" of the ingredient
+    pub amount: u8,
+    /// Option that burns the ingredient when crafting
+    pub burn_on_craft: bool,
 }
 
 /// Size: 32 + 1 = 33 bytes
 #[derive(Clone, AnchorSerialize, AnchorDeserialize)]
-pub struct Ingredient {
-    // Pubkey of the ingredient's token mint
+pub struct Item {
+    /// Pubkey of the item's token mint
     pub mint: Pubkey,
-    // Amount of the token required to satisy the "creation" of the ingredient
+    /// Amount of the token that will be minted on crat
     pub amount: u8,
 }
 
--- tests/createFormula.ts
+++ tests/createFormula.ts
@@ -1,14 +1,91 @@
-const anchor = require('@project-serum/anchor');
+import * as anchor from "@project-serum/anchor";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { AccountMeta, PublicKey, SystemProgram } from "@solana/web3.js";
+import {assert, expect} from 'chai';
+import { createIngredientMints, initNewTokenMint } from "./utils";
 
-describe('crafting', () => {
 
+describe('create_formula', () => {
+
+  const provider = anchor.Provider.env();
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
+  const program = anchor.workspace.Crafting;
+  const payer = anchor.web3.Keypair.generate();
+  // The mintAuthority for the ingredients
+  const mintAuthority = anchor.web3.Keypair.generate();
+  let ingredientMintA: PublicKey, ingredientMintB: PublicKey, outputMint: PublicKey;
+  before(async () => {
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(payer.publicKey, 10_000_000_000),
+      "confirmed"
+    );
+  })
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.Crafting;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-  });
+  describe("Two to one crafting", () => {
+    before(async () => {
+      // create the initial 2 mints (not owned by the user)
+      [ingredientMintA, ingredientMintB] = await createIngredientMints(
+        provider.connection,
+        mintAuthority.publicKey,
+        payer,
+        2
+      );
+      // TODO: create the 1 output mint which is owned by the user
+      const {mintAccount} = await initNewTokenMint(
+        provider.connection,
+        payer.publicKey,
+        payer,
+        0
+      );
+      outputMint = mintAccount.publicKey
+    })
+    it('should create a Formula', async () => {
+      const ingredients = [{
+        mint: ingredientMintA,
+        amount: 1,
+        burnOnCraft: true
+      },
+      {
+        mint: ingredientMintB,
+        amount: 1,
+        burnOnCraft: true
+      }];
+      const outputItems = [{
+        mint: outputMint,
+        amount: 1
+      }]
+
+      const remainingAccounts: AccountMeta[] = outputItems.map(x => ({
+        pubkey: x.mint,
+        isWritable: true,
+        isSigner: false
+      }))
+      const formulaKeypair = anchor.web3.Keypair.generate();
+      const expectedFormula = {
+        ingredients,
+        outputItems, 
+      }
+
+      await program.rpc.createFormula(
+        expectedFormula.ingredients.length,
+        expectedFormula.outputItems.length,
+        expectedFormula.ingredients,
+        expectedFormula.outputItems,
+        {
+          accounts: {
+            formula: formulaKeypair.publicKey,
+            authority: payer.publicKey,
+            tokenProgram: TOKEN_PROGRAM_ID,
+            systemProgram: SystemProgram.programId,
+          },
+          remainingAccounts,
+          signers: [payer, formulaKeypair],
+        }
+      );
+      let formula = await program.account.formula.fetch(formulaKeypair.publicKey)
+      expect(formula).to.eql(expectedFormula)
+    });
+  })
+
 });
